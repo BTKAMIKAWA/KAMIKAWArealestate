f0bbeb808c0d83acba464e2c9e1f9cd4a17bb18c
BTKAMIKAWA <briankamikawa@gmail.com>
2019-08-19 18:25:27 -0700

    localsettings

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'briankamikawa@gmail.com'
 EMAIL_HOST_PASSWORD = 'ZORAIMA1'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
